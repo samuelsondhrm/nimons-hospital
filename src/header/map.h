7ad795a8bbac88b986802e707270d57d6e838963
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-11 10:40:12 +0700

    Feat: Add map ADT

---
+++ src/header/map.h
@@ -0,0 +1,49 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include "Boolean.h"
+#include <stdio.h>
+#include <string.h> // If KeyType or ValueType are strings, this will be necessary for string operations.
+
+/* ------------- TIPE DATA MAP ------------- */
+typedef char KeyType[50];
+typedef int ValueType;
+typedef struct {
+    KeyType key;     // key untuk map
+    ValueType value; // value terkait dengan key
+} MapElType;
+
+#define CAPACITY 100 
+typedef struct {
+    MapElType elements[CAPACITY]; // Array untuk menyimpan elemen map
+    int count;                    // Jumlah elemen dalam map
+} Map;
+
+/* ------------- KONSTRUKTOR ------------- */
+// Fungsi ini untuk membuat map kosong (mengatur count ke 0). */
+void CreateEmptyMap(Map *m);
+
+/* ------------- PREDIKAT ------------- */
+// Fungsi ini mengembalikan true jika map kosong (count == 0), false jika tidak. 
+boolean IsMapEmpty(Map m);
+
+// Fungsi ini mengembalikan true jika map penuh (count == CAPACITY), false jika tidak.
+boolean IsMapFull(Map m);
+
+// Fungsi mencari key dalam array elemen map, jika ditemukan, mengembalikan true.
+boolean IsKeyMember(Map m, KeyType k);
+
+/* ------------- OPERASI DASAR ------------- */
+// Fungsi untuk menambahkan pasangan key dan value ke dalam map.
+void InsertMap(Map *m, KeyType k, ValueType v);
+
+// Fungsi akan mencari elemen dengan key yang sesuai, lalu menggeser elemen setelahnya untuk menghapusnya.
+void DeleteMap(Map *m, KeyType k);
+
+// Fungsi untuk mendapatkan value dari suatu key, diasumsikan bahwa key yang dicari pasti ada dalam map.
+ValueType GetValue(Map m, KeyType k);
+
+// Fungsi untuk mencetak seluruh isi map ke layar.
+void PrintMap(Map m);
+
+#endif
