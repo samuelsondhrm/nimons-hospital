c9d02e5352a81c0d550b55a817694581e3755663
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-11 09:43:19 +0700

    fix: changes towards ListUser is now brought over

--- src/header/parser_user_csv.h
+++ src/header/parser_user_csv.h
@@ -10,12 +10,12 @@ int string_to_int(const char *str);
 float string_to_float(const char *str);
 
 // Membaca isi file CSV dan menyimpannya ke array User, serta memperbarui jumlah user
-void parse_user_csv(const char *filename, User ListUser[], int *jumlah);
+void parse_user_csv(const char *filename, ListUser ListUser);
 
 // Menuliskan isi array User ke file CSV dengan format yang sama seperti dibaca
-void tulis_user_csv(const char *filename, User ListUser[], int jumlah);
+void tulis_user_csv(const char *filename, ListUser ListUser);
 
 // Menampilkan isi array User ke layar secara terformat
-void tampilkan_pasien(User ListUser[], int jumlah);
+void tampilkan_pasien(ListUser ListUser);
 
 #endif
