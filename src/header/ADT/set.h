4f8f26263c0861f6a6f68769fddd59fd088ccf62
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-20 23:27:52 +0700

    chore: Categorize ADT on src/header and src/c

---
+++ src/header/ADT/set.h
@@ -0,0 +1,72 @@
+/* File: set.h */
+/* Deklarasi ADT set yang berupa array satu dimensi berisikan elemen yang unik */
+
+#ifndef SET_H
+#define SET_H
+
+#include "Boolean.h"
+#include <stdio.h>
+
+#define MAX_SET_SIZE 100 // Maksimal jumlah elemen dalam set
+
+/* *** TIPE DATA SET *** */
+typedef int ElType;
+typedef struct {
+    ElType elements[MAX_SET_SIZE]; // Array untuk menyimpan elemen-elemen set
+    int count;                     // Jumlah elemen dalam set
+} Set;
+
+/* *** SELEKTOR MAKRO *** */
+#define SELMT(s, i) s.elements[i]
+#define COUNT(s) s.count
+
+/* *** KONSTRUKTOR *** */
+// Fungsi untuk membuat set kosong. 
+void CreateEmptySet(Set *s);
+
+/* *** PREDIKAT *** */
+// Mengembalikan true jika val ada dalam set s, false jika tidak.
+boolean IsMember(Set s, ElType val);
+
+// Mengembalikan true jika jumlah elemen dalam set mencapai MAX_SET_SIZE.
+boolean IsSetFull(Set s);
+
+// Mengembalikan true jika set tidak memiliki elemen (count == 0).
+boolean IsSetEmpty(Set s);
+
+/* *** OPERASI DASAR PADA SET *** */
+// Menambahkan val ke dalam set jika elemen tersebut belum ada dan set belum penuh.
+void InsertSet(Set *s, ElType val);
+
+// Menghapus elemen dengan nilai val pertama dari set jika ada.
+void DeleteSet(Set *s, ElType val);
+
+/* *** OPERASI LAIN *** */
+/* 
+ * Fungsi untuk mengembalikan union dari dua set.
+ * Union menghasilkan set baru yang berisi semua elemen unik dari kedua set.
+ * Contoh: {1,2,3} dan {2,3,4} menghasilkan {1,2,3,4}.
+ */
+Set SetUnion(Set s1, Set s2);
+
+/* 
+ * Fungsi untuk mengembalikan irisan (intersection) dari dua set.
+ * Intersection menghasilkan set baru yang berisi elemen yang ada di kedua set.
+ * Contoh: {1,2,3} dan {2,3,4} menghasilkan {2,3}.
+ */
+Set SetIntersection(Set s1, Set s2);
+
+/* 
+ * Fungsi untuk mengembalikan selisih (difference) antara dua set.
+ * Menghasilkan set baru yang berisi elemen di set s1 yang tidak ada di set s2.
+ * Contoh: {1,2,3} dan {2,3,4} menghasilkan {1}.
+ */
+Set SetDifference(Set s1, Set s2);
+
+/* 
+ * Fungsi untuk menampilkan isi set ke layar.
+ * Menampilkan set dalam format {a, b, c, ...}, tanpa spasi atau newline setelah '}'.
+ */
+void PrintSet(Set s);
+
+#endif
---
+++ src/header/ADT/user.h
@@ -0,0 +1,58 @@
+/* File: user.h */
+/* Deklarasi ADT User sebagai container data user.csv dan representasi akun */
+
+#ifndef USER_H
+#define USER_H
+
+#define MAX_FIELD 128
+
+// Struktur data untuk menyimpan informasi pasien
+typedef struct {
+    int id;
+    char username[MAX_FIELD];
+    char password[MAX_FIELD];
+    char role[MAX_FIELD];
+    char riwayat_penyakit[MAX_FIELD];
+    float suhu_tubuh;
+    int tekanan_darah_sistolik;
+    int tekanan_darah_diastolik;
+    int detak_jantung;
+    float saturasi_oksigen;
+    int kadar_gula_darah;
+    float berat_badan;
+    int tinggi_badan;
+    int kadar_kolesterol;
+    int kadar_kolesterol_ldl;
+    int trombosit;
+} User;
+
+typedef struct {
+    User users[MAX_FIELD];
+    int jumlahuser;
+} ListUser;
+
+/* ********** SELEKTOR ********** */
+// Makro untuk mempermudah akses elemen User
+#define ID(u) (u).id
+#define USERNAME(u) (u).username
+#define PASSWORD(u) (u).password
+#define ROLE(u) (u).role
+#define RIWAYAT_PENYAKIT(u) (u).riwayat_penyakit
+#define SUHU_TUBUH(u) (u).suhu_tubuh
+#define TEKANAN_SISTOLIK(u) (u).tekanan_darah_sistolik
+#define TEKANAN_DIASTOLIK(u) (u).tekanan_darah_diastolik
+#define DETAK_JANTUNG(u) (u).detak_jantung
+#define OKSIGEN(u) (u).saturasi_oksigen
+#define GULA_DARAH(u) (u).kadar_gula_darah
+#define BERAT_BADAN(u) (u).berat_badan
+#define TINGGI_BADAN(u) (u).tinggi_badan
+#define KOLESTEROL(u) (u).kadar_kolesterol
+#define KOLESTEROL_LDL(u) (u).kadar_kolesterol_ldl
+#define TROMBOSIT(u) (u).trombosit
+
+/* ********** KONSTRUKTOR ********** */
+// Membuat user kosong (default value)
+void CreateUser(User *u);
+void InitializeListUser(ListUser *l);
+
+#endif
