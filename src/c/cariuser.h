0cca69295fb4fb10310bec8fa7d06cdc62e044e9
samuelsondhrm <samuelsondharmawan31@gmail.com>
2025-05-21 02:54:45 +0700

    feat: add CariUser

---
+++ src/c/cariuser.h
@@ -0,0 +1,36 @@
+/*
+
+3 PROSEDUR PEMANGGILAN:
+    cariUser(&lu);
+    cariPasien(&lu);
+    cariDokter(&lu);
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "user.h"
+
+void toLowerCase(char *dest, const char *src);
+
+void swapUser(User *a, User *b);
+
+void sortUsers(User arr[], int n, int byId, int asc);
+
+int binarySearchById(User arr[], int n, int id);
+
+int sequentialSearchByName(User arr[], int n, const char *name);
+
+int searchPatientsByDisease(User arr[], int n, const char *disease, int result[]);
+
+void displayHeader(int mode);
+
+void displayUser(const User *u, int mode);
+
+void cariUser(ListUser *lu);
+
+void cariPasien(ListUser *lu);
+
+void cariDokter(ListUser *lu);
+
