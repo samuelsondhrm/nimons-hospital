f54eeec91c23009978876229274be2debfd014f9
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-21 20:55:34 +0700

    fix: add include guard into room.h

--- src/header/ADT/room.h
+++ src/header/ADT/room.h
@@ -1,5 +1,7 @@
 /* File: room.h */
 /* Deklarasi ADT Ruangan sebagai container config.txt */
+#ifndef ROOM_H
+#define ROOM_H
 
 #define MAX_PASIEN 100
 #define MAX_KOLOM_RUANGAN 26
@@ -16,3 +18,5 @@ typedef struct {
     Ruangan data[MAX_BARIS_RUANGAN][MAX_KOLOM_RUANGAN];    // Declaring ruangan di rumahsakit
     int kapasitasPerRuangan; // dibaca dari line 2 config file
 } RumahSakit;
+
+#endif
