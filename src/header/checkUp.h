3aaf5d360600f565b1f73ef590c004a288a90c0d
unknown <raf14kbar9a@gmail.com>
2025-05-26 04:28:02 +0700

    feat: slight change to checkUp.h

--- src/header/checkUp.h
+++ src/header/checkUp.h
@@ -1,9 +1,13 @@
 #ifndef CHECKUP_H
 #define CHECKUP_H
 
+#define CAPACITY 100
+
 #include "parser_user_csv.h"  
 #include "queue.h"            
-#include "room.h"             
+#include "room.h"   
+
+extern int pasienAntri[CAPACITY];
 
 void DaftarCheckUp(ListUser *listUser, Queue *antrianDokter, RumahSakit *rs, const char *username);
 
