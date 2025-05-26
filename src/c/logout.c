ef4c44c394881aa876455ddbd5304d22e687b4dc
Rhen <rhenitb@gmail.com>
2025-05-27 05:40:21 +0700

    feat: add current_user reset and update header

--- src/c/logout.c
+++ src/c/logout.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
 #include "login.h"
+#include "logout.h"
 
-int main() {
-    char instruksi[MAX_FIELD];
-
-    printf(">>> ");
-    scanf("%s", instruksi);
-
-    if (strcmp(instruksi, "LOGOUT") == 0) { 
+void logout() {
         if (SudahLogin) {
             printf("Sampai jumpa!\n");
             SudahLogin = false;
+            current_user = NULL;
         }
         else {
             printf("Logout gagal!\n");
             printf("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout\n");
         }  
     }
-
-    return 0;
-}
 
