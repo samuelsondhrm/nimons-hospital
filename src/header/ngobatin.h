c2f21bbdb63e5533ac8da7c2ae9e350c73d7bb92
unknown <raf14kbar9a@gmail.com>
2025-05-27 10:32:45 +0700

    feat: implementasi fitur ngobatin

---
+++ src/header/ngobatin.h
@@ -0,0 +1,13 @@
+#ifndef NGOBATIN_H
+#define NGOBATIN_H
+
+#include "../src/header/ADT/user.h"
+#include "../src/header/ADT/penyakit_dan_obat.h"
+#include "../src/header/ADT/map.h"
+#include "../src/header/ADT/liststatik.h"
+#include "../src/header/ADT/room.h"
+#include <stdbool.h>
+
+void ngobatin(User current_user, RumahSakit *rs, ListUser *lUser, ListObat *lObat, ListFormula *lFormula, ListPenyakit *lPenyakit);
+
+#endif
