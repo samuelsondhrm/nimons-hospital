93e90e8aac3f357ec42a7fdc72bd4e32f9b62aa2
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-28 21:09:38 +0700

    feat: add RumahSakit, Pasien, Inventory, Ruangan

---
+++ src/c/ADT/room.c
@@ -0,0 +1,22 @@
+#include "../../header/ADT/room.h"
+void InitializeRumahSakit(RumahSakit *rs){
+    rs->rows = 0;
+    rs->cols = 0;
+    for(int i = 0; i < MAX_BARIS_RUANGAN; i++){
+        for(int j = 0; j < MAX_KOLOM_RUANGAN; j++){
+            rs->data[i][j].dokterId = 0;
+            rs->data[i][j].jumlahPasien = 0;
+            CreateQueue(&rs->data[i][j].baris);
+        }
+    }
+    rs->kapasitasPerRuangan = 0;
+    rs->kapasitasBaris = 0;
+}
+void InitializePasien(Pasien *P){
+    P->jumlahobat = 0;
+    CreateEmpty(&P->perut);
+}
+
+void InitializeInventory(Inventory *I){
+    I->jumlahPasienwObat = 0;
+}
