7cd66c6eeeb1cdc7c235ea44799bc815332ddd86
samuelsondhrm <samuelsondharmawan31@gmail.com>
2025-05-29 18:39:28 +0700

    fix: changing code into a queue data type based program

--- src/c/ADT/room.c
+++ src/c/ADT/room.c
@@ -1,23 +1,24 @@
 #include "../../header/ADT/room.h"
 
-void InitializeRumahSakit(RumahSakit *rs){
+void InitializeRumahSakit(RumahSakit *rs) {
     rs->rows = 0;
     rs->cols = 0;
-    for(int i = 0; i < MAX_BARIS_RUANGAN; i++){
-        for(int j = 0; j < MAX_KOLOM_RUANGAN; j++){
-            rs->data[i][j].dokterId = 0;
-            rs->data[i][j].jumlahPasien = 0;
-            CreateQueue(&rs->data[i][j].baris);
-        }
-    }
     rs->kapasitasPerRuangan = 0;
     rs->kapasitasBaris = 0;
+
+    for (int i = 0; i < MAX_BARIS_RUANGAN; i++) {
+        for (int j = 0; j < MAX_KOLOM_RUANGAN; j++) {
+            rs->data[i][j].dokterId = 0;
+            CreateQueue(&rs->data[i][j].antrianPasienIds);
+        }
+    }
 }
-void InitializePasien(Pasien *P){
+
+void InitializePasien(Pasien *P) {
     P->jumlahobat = 0;
     CreateEmptyStack(&P->perut);
 }
 
-void InitializeInventory(Inventory *I){
+void InitializeInventory(Inventory *I) {
     I->jumlahPasienwObat = 0;
 }
