8a2e229a06eea1dad4524ce7bf7bc5c34b74c382
Rhen <rhenitb@gmail.com>
2025-05-30 05:14:51 +0700

    refactor: sync with updated structs in room.h

--- src/c/pulangDok.c
+++ src/c/pulangDok.c
@@ -1,136 +1,119 @@
 #include "../header/pulangDok.h"
 
-void salinPerutKeArray(Stack s, int arr[], int *count) {
-    *count = 0;
-    Stack temp;
-    CreateEmptyStack(&temp);
-    while (!IsEmptyStack(s)) {
-        int val;
-        Pop(&s, &val);
-        temp.T[temp.TOP + 1] = val;
-        temp.TOP++;
-    }
-    while (!IsEmptyStack(temp)) {
-        int val;
-        Pop(&temp, &val);
-        arr[*count] = val;
-        (*count)++;
-    }
-}
-
-int cariPenyakitId(ListPenyakit lp, const char *nama) {
-    for (int i = 0; i < JUMLAH_PENYAKIT(lp); i++) {
-        if (strcmp(NAMA_PENYAKIT(PENYAKIT_LIST(lp, i)), nama) == 0) {
-            return ID_PENYAKIT(PENYAKIT_LIST(lp, i));
-        }
-    }
-    return -1;
-}
-
-void ambilResepUrut(ListFormula lf, int penyakitId, int resep[], int *count) {
-    *count = 0;
-    for (int urut = 1; urut <= MAX_OBAT; urut++) {
-        for (int i = 0; i < JUMLAH_FORMULA(lf); i++) {
-            Formula f = FORMULA_LIST(lf, i);
-            if (f.penyakit_id == penyakitId && f.urutan == urut) {
-                resep[*count] = f.obat_id;
-                (*count)++;
-            }
-        }
-    }
-}
-
-void cetakUrutanObat(int list[], int len, ListObat *lObat) {
-    for (int i = 0; i < len; i++) {
-        Obat o = GetObat(*lObat, list[i]);
-        printf("%s", o.nama);
-        if (i < len - 1) {
-            printf(" -> ");
-        }
-    }
-    printf("\n");
-}
-
-boolean samaUrutan(int a[], int na, int b[], int nb) {
-    if (na != nb) return false;
-    for (int i = 0; i < na; i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-void pulangDok(User current_user, RumahSakit *rs, ListUser *lUser, Inventory *inv, ListFormula *lFormula, ListPenyakit *lPenyakit, ListObat *lObat) {
+void bolehPulang(User current_user, Inventory *inv, const ListObat *lObat, const ListFormula *lFormula, const ListPenyakit *lPenyakit, RumahSakit *rs) {
     int pasienId = USER_ID(current_user);
 
-    if (strcmp(RIWAYAT_PENYAKIT(current_user), "-") == 0 || strlen(RIWAYAT_PENYAKIT(current_user)) == 0) {
+    // Kasus 1: Belum menerima diagnosis
+    if (!SudahDiagnosis[pasienId]) {
         printf("Kamu belum menerima diagnosis apapun dari dokter, jangan buru-buru pulang!\n");
         return;
     }
 
-    printf("Dokter sedang memeriksa keadaanmu...\n");
-
-    Pasien *pasien = NULL;
+    int idxInv = -1;
     for (int i = 0; i < inv->jumlahPasienwObat; i++) {
         if (inv->data[i].id == pasienId) {
-            pasien = &inv->data[i];
+            idxInv = i;
             break;
         }
     }
 
-    if (!pasien) {
-        printf("Data pasien tidak ditemukan di inventory.\n");
-        return;
-    }
-
-    int penyakitId = cariPenyakitId(*lPenyakit, RIWAYAT_PENYAKIT(current_user));
-
-    if (penyakitId == -1) {
-        printf("Riwayat penyakit tidak dikenali.\n");
-        return;
-    }
-
-    int resep[MAX_OBAT], nResep;
-    ambilResepUrut(*lFormula, penyakitId, resep, &nResep);
-
-    int perut[MAX_OBAT], nPerut;
-    salinPerutKeArray(pasien->perut, perut, &nPerut);
-
-    // Kasus 2: belum habis
-    if (nPerut < nResep) {
+    // Kasus 2: Masih ada obat di inventory atau belum minum apa-apa
+    if (idxInv == -1) {
+        printf("Dokter sedang memeriksa keadaanmu...\n");
         printf("Masih ada obat yang belum kamu habiskan, minum semuanya dulu yukk!\n");
         return;
     }
 
-    // Kasus 3: salah urutan
-    if (!samaUrutan(resep, nResep, perut, nPerut)) {
-        printf("Maaf, tapi kamu masih belum bisa pulang!\n");
-        printf("Urutan peminuman obat yang diharapkan:\n");
-        cetakUrutanObat(resep, nResep, lObat);
-        printf("Urutan obat yang kamu minum\n");
-        cetakUrutanObat(perut, nPerut, lObat);
-        printf("Silahkan kunjungi dokter untuk meminta penawar yang sesuai !\n");
+    Stack *perut = &inv->data[idxInv].perut;
+
+    if (inv->data[idxInv].jumlahobat > 0 || IsEmptyStack(*perut)) {
+        printf("Dokter sedang memeriksa keadaanmu...\n");
+        printf("Masih ada obat yang belum kamu habiskan, minum semuanya dulu yukk!\n");
         return;
     }
 
-    // Kasus 4: sembuh
-    printf("Selamat! Kamu sudah dinyatakan sembuh oleh dokter. Silahkan pulang dan semoga sehat selalu!\n");
+    // Kasus 3: Urutan obat tidak sesuai
+    const char *penyakitPasien = RIWAYAT_PENYAKIT(current_user);
+    ListStatik obatSeharusnya;
+    CreateListStatik(&obatSeharusnya);
 
-    // Hapus pasien dari ruangan
-    for (int i = 0; i < rs->rows; i++) {
-        for (int j = 0; j < rs->cols; j++) {
-            Ruangan *r = &rs->data[i][j];
-            for (int k = 0; k < r->jumlahPasien; k++) {
-                if (r->pasienIds[k] == pasienId) {
-                    for (int l = k; l < r->jumlahPasien - 1; l++) {
-                        r->pasienIds[l] = r->pasienIds[l + 1];
-                    }
-                    r->jumlahPasien--;
-                    break;
-                }
+    for (int i = 0; i < JUMLAH_FORMULA(*lFormula); i++) {
+        Formula f = FORMULA_LIST(*lFormula, i);
+
+        for (int j = 0; j < JUMLAH_PENYAKIT(*lPenyakit); j++) {
+            Penyakit p = PENYAKIT_LIST(*lPenyakit, j);
+            if (ID_PENYAKIT(p) == f.penyakit_id && strcmp(NAMA_PENYAKIT(p), penyakitPasien) == 0) {
+                insertLastList(&obatSeharusnya, f.obat_id);
+                break;
             }
         }
     }
 
-    CreateEmptyStack(&pasien->perut);
-    pasien->jumlahobat = 0;
+    boolean urutanBenar = true;
+    Stack salinanPerut = *perut;
+
+    if (listLength(obatSeharusnya) != salinanPerut.TOP + 1) {
+        urutanBenar = false;
+    } else {
+        for (int i = listLength(obatSeharusnya) - 1; i >= 0; i--) {
+            int top;
+            Pop(&salinanPerut, &top);
+            if (top != ELMT_LIST(obatSeharusnya, i)) {
+                urutanBenar = false;
+                break;
+            }
+        }
+    }
+
+    if (!urutanBenar) {
+        printf("Dokter sedang memeriksa keadaanmu...\n");
+        printf("Maaf, tapi kamu masih belum bisa pulang!\n");
+        printf("Urutan peminuman obat yang diharapkan:\n");
+        for (int i = 0; i < listLength(obatSeharusnya); i++) {
+            Obat o = GetObat(*lObat, ELMT_LIST(obatSeharusnya, i));
+            printf("%s", NAMA_OBAT(o));
+            if (i != listLength(obatSeharusnya) - 1) printf(" -> ");
+        }
+        printf("\n");
+
+        printf("Urutan obat yang kamu minum:\n");
+        Stack tampilPerut = *perut;
+        while (!IsEmptyStack(tampilPerut)) {
+            int id;
+            Pop(&tampilPerut, &id);
+            Obat o = GetObat(*lObat, id);
+            printf("%s", NAMA_OBAT(o));
+            if (!IsEmptyStack(tampilPerut)) printf(" -> ");
+        }
+        printf("\n");
+
+        printf("Silahkan kunjungi dokter untuk meminta penawar yang sesuai!\n");
+        return;
+    }
+
+    // Kasus 4: Semua syarat terpenuhi
+    printf("Dokter sedang memeriksa keadaanmu...\n");
+    printf("Selamat! Kamu sudah dinyatakan sembuh oleh dokter. Silahkan pulang dan semoga sehat selalu!\n");
+
+    // Reset status
+    SudahDiagnosis[pasienId] = false;
+    inv->data[idxInv].jumlahobat = 0;
+    CreateEmptyStack(perut);
+
+    for (int i = 0; i < rs->rows; i++) {
+        for (int j = 0; j < rs->cols; j++) {
+            Queue *q = &rs->data[i][j].antrianPasienIds;
+            int kapasitas = rs->kapasitasPerRuangan;
+            int len = lengthQueue(*q);
+
+            for (int k = 0; k < kapasitas && k < len; k++) {
+                int idx = (q->idxHead + k) % CAPACITY_QUEUE;
+                if (q->buffer[idx] == pasienId && k == 0) {
+                    int pasienKeluar;
+                    Dequeue(q, &pasienKeluar);
+                    return;
+                }
+            }
+        }
+    }
 }
--- src/header/pulangDok.h
+++ src/header/pulangDok.h
@@ -1,23 +1,29 @@
 #ifndef PULANGDOK_H
 #define PULANGDOK_H
 
-#include <stdio.h>
-#include <string.h>
-#include "ADT/room.h"
-#include "ADT/stack.h"
 #include "ADT/user.h"
 #include "ADT/penyakit_dan_obat.h"
+#include "ADT/stack.h"
+#include "ADT/queue.h"
+#include "ADT/room.h"
+#include "ADT/liststatik.h"
+#include "diagnosis.h"
 
-void salinPerutKeArray(Stack s, int arr[], int *count);
+/*
+Mengecek apakah pasien sudah bisa dipulangkan berdasarkan diagnosis dan urutan konsumsi obat.
+Syarat:
+- Sudah didiagnosis
+- Semua obat sudah diminum (inventory kosong)
+- Urutan peminuman obat sesuai dengan formula penyakit
+ 
+Jika syarat dipenuhi:
+- Pasien dikeluarkan dari ruangan
+- Perut (stack) dan inventory direset
+- Diagnosis diselesaikan
+ 
+Jika tidak, akan menampilkan alasan dan saran sesuai spesifikasi.
+*/
 
-int cariPenyakitId(ListPenyakit lp, const char *nama);
-
-void ambilResepUrut(ListFormula lf, int penyakitId, int resep[], int *count);
-
-void cetakUrutanObat(int list[], int len, ListObat *lObat);
-
-boolean samaUrutan(int a[], int na, int b[], int nb);
-
-void pulangDok(User current_user, RumahSakit *rs, ListUser *lUser, Inventory *inv, ListFormula *lFormula, ListPenyakit *lPenyakit, ListObat *lObat);
+void bolehPulang(User current_user, Inventory *inv, const ListObat *lObat, const ListFormula *lFormula, const ListPenyakit *lPenyakit, RumahSakit *rs);
 
 #endif
