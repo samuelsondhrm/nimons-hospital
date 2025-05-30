fb1a516b2f699041abf8391a7726e627e5055052
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-30 19:34:03 +0700

    feat: display user info

--- src/header/interface.h
+++ src/header/interface.h
@@ -4,11 +4,13 @@
 #define INTERFACE_H
 #include <stdio.h>
 #include <string.h>
+#include "ADT/user.h"
 #ifdef _WIN32
 #include <windows.h>  // buat Sleep()
 #else
 #include <unistd.h>   // buat  usleep()
 #endif
+
 
 #define MERAH     "\033[0;31m"
 #define HIJAU   "\033[0;32m"
@@ -77,10 +79,10 @@ void animate_text(const char *text, int delay_ms);
 void print_color(const char *text, const char *color_code);
 
 // Fungsi untuk print header
-void print_header();
+void print_header(User active_user);
 
 // Fungsi untuk print fungsi
-void print_case(const char *func_name);
+void print_case(const char *func_name, User active_user);
 
 
 #endif
