ce51710278a8923681a599cae53f0338adb3e2d6
unknown <raf14kbar9a@gmail.com>
2025-06-02 20:08:04 +0700

    feat: penambahan data ke List user

--- src/header/register.h
+++ src/header/register.h
@@ -8,6 +8,7 @@
 
 #define MAX_FIELD 128
 
+void InsUser(ListUser *lUser, User userBaru);
 int compare_case_insensitive(const char* a, const char* b);
 boolean is_username_valid(const char* username);
 void register_pasien(ListUser *lUser, User *new_user);
